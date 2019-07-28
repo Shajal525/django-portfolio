0d23684e61d09398ea197d9bc3b498e79e4fe585
shajal525 <shajal525@gmail.com>
2019-07-28 23:19:12 +0600

    Homepage almost completed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class blog(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	publisher = models.CharField(max_length=200)
+	pub_date = models.DateTimeField()
+	image = models.ImageField(upload_to='images/')
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import job
+
+def home(request):
+	allJobs = job.objects
+
+	return render(request, 'jobs/home.html', {'jobs': allJobs})
